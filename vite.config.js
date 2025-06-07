8b500592e2cb5f8034fb429282fa076955e5237c
Eswar <eswarbev05@gmail.com>
2025-06-07 14:04:03 +0530

    version 2.1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
